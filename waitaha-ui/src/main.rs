18aff0d5c884856c5d107003d5e7e97d5f7b9ed4
Jens Reidel <adrian@travitia.xyz>
2024-08-06 08:42:23 +0200

    waitaha-ui: Make the demo more readable
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- waitaha-ui/src/main.rs
+++ waitaha-ui/src/main.rs
@@ -1,15 +1,7 @@
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+    let app = MainWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
-
-    ui.run()
+    app.run()
 }
